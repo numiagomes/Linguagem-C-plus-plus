9292204695fe9d15415af25eb889b3f570a2464e
Numiá Freitas Gomes <37479975+numiagomes@users.noreply.github.com>
2018-09-19 21:26:33 -0300

    Create lista_de_exercícios.cpp

---
+++ lista_de_exercícios.cpp
@@ -0,0 +1,13 @@
+//exercício 9
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int var = 0;
+    int controle=0;
+    int num_iteracoes = 20;
+    for (controle = 0; controle<num_iteracoes;controle++){
+        var = var+2;
+    }
+    cout<<var;}
