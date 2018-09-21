b336383b7db30d0dfaa8348ccb4b533e6357fe34
Numiá Freitas Gomes <37479975+numiagomes@users.noreply.github.com>
2018-09-21 20:33:48 -0300

    Update lista_de_exercícios.cpp

--- lista_de_exercícios.cpp
+++ lista_de_exercícios.cpp
@@ -1,3 +1,100 @@
+
+//Lista de Exercícios
+
+/*Exercício 1 - Crie um programa em C++, que recebe uma palavra como parâmetro e imprime.*/
+
+/*Exercício 2 - Crie um programa em C++ que atribui o valor 20 para uma variável inteira, o valor
+30 para outra variável inteira e some esses dois valores e armazene em outra variável.*/
+
+/*Exercício 3 - Crie um programa em C++ com uma variável do tipo String, contendo um nome qualquer.
+Crie uma segunda variável do tipo Float, contendo o valor do salário dessa pessoa. Imprima:
+
+A pessoa <Nome da Pessoa>, recebe R$<Salário da pessoa>.
+
+Exercício 4 - Crie um programa em C++ que compara o valor do salário de duas pessoas, e informa
+qual salário é maior.
+
+Dica - Utilize a instrução if para fazer comparações e os operadores > ou < para comparar.*/
+
+/*Exercício 5 - Crie um programa em C++ que recebe a pontuação de um usuário e a de outro usuário
+e verifica quem ganhou o jogo, com base na maior pontuação.*/
+
+/*Exercício 6 - Crie um vetor de elementos do tipo inteiro e imprima o primeiro e o último elemento
+desse vetor.*/
+
+/*Exercício 7 - Crie um vetor de elementos do tipo float, insira valores nesses elementos e faça um
+loop para imprimir cada um desses elementos.*/
+
+/*Exercício 8 - Três times de futebol participaram de 6 partidas. Crie vetores para simular os resultados
+desses jogos e imprima a pontuação obtida por cada time.*/
+
+/*Exercício 9 - Cria um programa em C++ que possui uma variável inteira que se inicia com o valor 0.
+Faça com que ela acumule o valor 2 nela mesma ao longo de 20 iterações.*/
+
+/*Dica: Em um loop podemos fazer uma variável receber o valor dela mesma mais o valor que se deseja
+acumular.*/
+
+/*Exercício 10 - Pensando na mesma lógica do programa anterior, crie uma alteração no Exercício referente
+aos times, para que seja feita a soma das pontuações de cada um.*/
+
+/*Revisão de Função*/
+
+/*Exercício 11 - Crie um programa que faz a leitura da nota final de um aluno e verifica se ele 
+foi aprovado, reprovado ou se está de exame especial em uma disciplina. Os critérios que devem
+ser considerados são: Nota acima de 70 indica aprovação, nota abaixo de 40 indica reprovação e
+nota entre 40 e 59 indica exame especial.*/
+
+/*Dicas: Utilize o cin, para fazer a leitura da nota, conforme mostrado em sala.
+Utilize condições com o comando if para verificar a nota lida pelo comando cin.
+É indicado que seja feita uma função para verificar a situação do aluno. Uma das formas
+é a função receber a nota do aluno como parâmetro e retornar um string contendo um texto
+referente à situação do aluno.*/
+
+/*Exercício 12 - Faça uma alteração no seu programa anterior, para que ele utilize as notas individuais
+do aluno em cada atividade da disciplina, e calcule o resultado final, para que posteriormente possa
+ser verificada a aprovação do aluno.*/
+
+/*Dixas: Crie um vetor, contendo um conjunto de notas. Faça um loop que percorre esse conjunto de
+notas e faz o seu somatório.
+Depois utilize a função criada no exercício 1, para verificar a situação do aluno.*/
+
+/*Ecercício 13 - Um desenvolvedor recebe 50 reais por hora trabalhada. Considerando que ele trabalhada
+8 horas por dia, crie uma função que calcula quanto esse desenvolvedor recebeu no mês atual, considerando
+que ele trabalhou 22 dias nesse mês.*/
+
+/*Exercício 14 - Uma empresa de aviação possui um tipo de avião que comporta 10 pessoas. Crie um programa
+que possui uma função, cujos parâmetros de entrada são: o número de aviões disponíveis e o número de
+passageiros para serem transportados. Essa função deverá calcular se os aviões conseguem comportar essas
+pessoas, e caso não consiga, quantas ficarão para as próximas viagens. Faça uma simulação na qual os
+aviões comportam o número de pessoas e outra na qual ele não comporta.*/
+
+/*Exercício 15 - Um hospital consegue atender 20 pessoas por hora. Considerando que no momento em que
+o hospital foi aberto chegaram 100 pessoas, faça um programa para calcular quanto tempo o paciente com
+a senha 68 irá demorar para ser atendido.*/
+
+/*Dicas: Faça um loop while, que inicia com 20 pessoas sendo atendidas sem espera, e caminha de 20 em 20.
+A cada iteração ele deverá aumentar em 20 o número de pessoas sendo atendidas e em 1 o número de horas.
+Caso o número de pessoas atendidas seja maior do que o número da senha que se deseja verificar, isso
+indica que aquele paciente entrou em atendimento na iteração atual, e esperou o número de horas 
+acumuladas até o momento.*/
+
+/*Exercício 16 - Faça um programa em C++ que percorre um conjunto de números e informa qual deles é o
+maior.*/
+
+/*Exercício 17 - Faça um programa em C++ que executa a mesma operação do primeiro, porém informa a
+posição do maior número, e não o seu valor.*/
+
+/*Exercício 18 - Faça um ajuste no programa 16, para que ele também informe o menor valor da lista.*/
+
+/*Exercício 19 - A equipe de uma contrutora consegue entregar 100 metros quadrados construídos por dia.
+Com base nisso, faça um programa que possui uma função para efetuar o cálculo do tempo necessário
+para construir um projeto com 2326 metros quadrados. Esse tempo deve ser fornecido em dias.*/
+
+/*Exercício 20 - Gere um conjunto de 10 números inteiros de forma aleatória, e verifique qual é o maior
+valor gerado.*/
+
+
+
 //exercício 9
 #include <iostream>
 
