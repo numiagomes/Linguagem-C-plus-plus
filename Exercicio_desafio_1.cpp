5afb07bbafb6f4fe09582f20e634ff4a0f9cf7d2
Numiá Freitas Gomes <37479975+numiagomes@users.noreply.github.com>
2018-09-24 08:40:42 -0300

    Update Exercicio_desafio_1.cpp

--- Exercicio_desafio_1.cpp
+++ Exercicio_desafio_1.cpp
@@ -1,1 +1,20 @@
+/*Uma empresa precisa cadastrar alguns dados de seus funcionários e fazer alguns cálculos.
+O primeiro requisito é o preeenchimento de informações do funcionário. Para isso, implemente uma coleta de informações, para os seguintes dados:
+nome, cpf, nacionalidade, identidade, pais, estado, cidade, rua, número e salário.
+Uma vez que as informações foram fornecidas, verifique se o funcionário é do Braisl. Caso seja, imprima um texto com essa informaçãopara o funcionário do RH.
+Faça o cálculo do desconto em folha, devido ao INSS, segundo a tabela abaixo:
+ Salário (R$)              Desconto
+ até 1693,72                 8%
+ de 1693,72 até 2822,90      9%
+ acima de 2.822,91           11% 
+ Imprima todos os dados do funcionário. Verifique será o cargo na carteira de trabalho do funcionário
+ Level 1 - salário até 1500 - estágiario
+ Leval 2 -  de 1501 a 2000 - dev junior
+ level 3 - de 2001 a 4000 - dev pleno
+ level 4 - de 4001 a 7000 - dev sênior
+ leval 5 - de 7001 a 1000 - GP 
+ Level 6 -  de 10001 a 1500 - GP sênior
+ Level 7 - de 15001 a 30000 - diretor de ti
+ level 8 - 30001 a 50000 - diretor de ti sênior
+ level 9 - 50001 a 100000 - Presidente*/
 
