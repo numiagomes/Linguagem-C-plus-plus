50237fb42d6fa98edc5e8fd39d8a8b931802f1ec
Numiá Freitas Gomes <37479975+numiagomes@users.noreply.github.com>
2018-09-25 15:35:05 -0300

    Update Exercicio_desafio_1.cpp

--- Exercicio_desafio_1.cpp
+++ Exercicio_desafio_1.cpp
@@ -2,13 +2,13 @@
 O primeiro requisito é o preeenchimento de informações do funcionário. Para isso, implemente uma coleta de informações, para os seguintes dados:
 nome, cpf, nacionalidade, identidade, pais, estado, cidade, rua, número e salário. OK! (verificar pq o nome e complemento não aceita espaço)
 Uma vez que as informações foram fornecidas, verifique se o funcionário é do Braisl. Caso seja, imprima um texto com essa informação
-para o funcionário do RH.OK! 
+para o funcionário do RH.OK! (resolver bug de leitura dos numeros em cpf e identidade)
 Faça o cálculo do desconto em folha, devido ao INSS, segundo a tabela abaixo:
  Salário (R$)              Desconto
  até 1693,72                 8%
  de 1693,72 até 2822,90      9%
- acima de 2.822,91           11% 
- Imprima todos os dados do funcionário. Verifique será o cargo na carteira de trabalho do funcionário
+ acima de 2.822,91           11% ok!
+ Imprima todos os dados do funcionário. Verifique qual será o cargo na carteira de trabalho do funcionário
  Level 1 - salário até 1500 - estágiario
  Leval 2 -  de 1501 a 2000 - dev junior
  level 3 - de 2001 a 4000 - dev pleno
@@ -52,15 +52,15 @@ float cadastro (string nome, double cpf, string nacionalidade, int identidade,st
     cin >> proximo;
     }
  if ( pais == "Brasil"){
-        cout << "Este funcionário é residente do " <<pais;
+        cout << "Este funcionário é residente do " <<pais<<"\n";
  }
- if (salario <= 1693.72, pais == "Brasil""\n"){
+ if (salario <= 1693.72 && pais == "Brasil"){
             desconto = (salario * 0.08);
  }
- else if (1693.73 < salario >= 2822.90){
+ else if (salario > 1693.72 && salario <= 2822.90 && pais == "Brasil"){
                 desconto = (salario * 0.09);
  }
- else if ( salario > 2822.90){
+ else if ( salario > 2822.90 && pais == "Brasil"){
                 desconto = (salario * 0.11);
  }
  else {desconto = 0;
@@ -68,8 +68,36 @@ float cadastro (string nome, double cpf, string nacionalidade, int identidade,st
  cout << "O funcionário " << nome<< " que possui CPF "<< cpf<< " e identidade "<< identidade<<"."<< nacionalidade<< " residente no seguinte endereço rua "<<rua<<", "<< 
  numero<<", "<<complemento<<", "<<cidade<<", "<<estado<<", "<<pais<<"\n" "Tem salário " << salario<< " e seu desconto em folha para o INSS será: "<< desconto;
 
+ if (salario <= 1500){
+     cout<< " Este funcionário é classificado como estágiario";
+ }
+ else if (salario > 1501 && salario <= 2000){
+      cout<< " Este funcionário é classificado como desenvolvedor junior";
+ }
+ else if(salario > 2001 && salario <= 4000){
+      cout<< " Este funcionário é classificado como desenvolvedor pleno";
+ }
+  else if(salario > 4001 && salario <= 7000){
+      cout<< " Este funcionário é classificado como desenvolvedor sênior";
+ }
+   else if(salario > 7001 && salario <= 10000){
+      cout<< " Este funcionário é classificado como gerente de projetos";
+ }
+   else if(salario > 10001 && salario <= 15000){
+      cout<< " Este funcionário é classificado como gerente de projetos sênior";
+ }
+    else if(salario > 15001 && salario <= 30000){
+      cout<< " Este funcionário é classificado como diretor de TI";
+ }
+     else if(salario > 30001 && salario <= 50000){
+      cout<< " Este funcionário é classificado como diretor de TI sênor";
+ }
+ else {cout<< " Este é o Presidente da empresa";
+     
+ }
     return 0;
 }
+
 int main(){ 
     string nome ="";
     double cpf = 0;
