f5874e41b84c798044acd95c89ddc419d87cb3ec
Numiá Freitas Gomes <37479975+numiagomes@users.noreply.github.com>
2018-09-25 16:38:25 -0300

    Update Exercicio_desafio_1.cpp

--- Exercicio_desafio_1.cpp
+++ Exercicio_desafio_1.cpp
@@ -24,21 +24,25 @@ Faça o cálculo do desconto em folha, devido ao INSS, segundo a tabela abaixo:
 
 using namespace std;
 
-float cadastro (string nome, double cpf, string nacionalidade, int identidade,string pais, string estado, string cidade, string rua, int numero, string complemento, double salario, string proximo, double desconto){
+#include <iostream>
+
+using namespace std;
+
+float cadastro (string nome, long int cpf, string nacionalidade, long int identidade,string pais, string estado, string cidade, string rua, int numero, string complemento, double salario, string proximo, double desconto){
     while (proximo != "não"){
    cout << " Preencha as informações refente ao funcionário.""\n" "Nome completo:""\n";
     getline (cin,nome);
     cin.ignore();
     cout << "CPF (somente os números):""\n";
     cin >> cpf;
-    cout << "Nacionalidade:";
+    cout << "Nacionalidade:""\n";
     getline (cin, nacionalidade);
-    
+    cin.ignore();
     cout << "Indentidade:""\n";
     cin >> identidade;
     cout << "Endereço completo""\n""País:""\n";
     getline (cin,pais);
-    
+    cin.ignore();
     cout << "Estado:""\n";
     getline (cin,estado);
     cout << "Cidade:""\n";
@@ -48,16 +52,17 @@ float cadastro (string nome, double cpf, string nacionalidade, int identidade,st
     cin.ignore();
     cout << "Número:""\n";
     cin >> numero;
+    cin.ignore();
     cout << "Complemento:""\n";
     getline (cin,complemento);
-    
+    cin.ignore();
     cout << "Salário:""\n";
     cin >> salario;
     cout << "Deseja cadastrar outro funcinário? ( sim ou não)""\n";
     cin >> proximo;
     }
  if ( pais == "Brasil"){
-        cout << "Este funcionário é residente do " <<pais<<"\n";
+        cout << "Este funcionário reside no " <<pais<<".""\n";
  }
  if (salario <= 1693.72 && pais == "Brasil"){
             desconto = (salario * 0.08);
@@ -70,34 +75,34 @@ float cadastro (string nome, double cpf, string nacionalidade, int identidade,st
  }
  else {desconto = 0;
       }
- cout << "O funcionário " << nome<< " que possui CPF "<< cpf<< " e identidade "<< identidade<<"."<< nacionalidade<< " residente no seguinte endereço rua "<<rua<<", "<< 
- numero<<", "<<complemento<<", "<<cidade<<", "<<estado<<", "<<pais<<"\n" "Tem salário " << salario<< " e seu desconto em folha para o INSS será: "<< desconto;
+ cout << "O funcionário " << nome<< " que possui CPF "<< cpf<< " e identidade "<< identidade<<". "<< nacionalidade<< " residente na "<<rua<<", "<< 
+ numero<<", "<<complemento<<", "<<cidade<<", "<<estado<<", "<<pais<<"\n" "Tem salário " << salario<< " e seu desconto em folha para o INSS será: "<< desconto<<".";
 
  if (salario <= 1500){
-     cout<< " Este funcionário é classificado como estágiario";
+     cout<< " Este funcionário é classificado como estágiario.";
  }
  else if (salario > 1501 && salario <= 2000){
-      cout<< " Este funcionário é classificado como desenvolvedor junior";
+      cout<< " Este funcionário é classificado como desenvolvedor junior.";
  }
  else if(salario > 2001 && salario <= 4000){
-      cout<< " Este funcionário é classificado como desenvolvedor pleno";
+      cout<< " Este funcionário é classificado como desenvolvedor pleno.";
  }
   else if(salario > 4001 && salario <= 7000){
-      cout<< " Este funcionário é classificado como desenvolvedor sênior";
+      cout<< " Este funcionário é classificado como desenvolvedor sênior.";
  }
    else if(salario > 7001 && salario <= 10000){
-      cout<< " Este funcionário é classificado como gerente de projetos";
+      cout<< " Este funcionário é classificado como gerente de projetos.";
  }
    else if(salario > 10001 && salario <= 15000){
-      cout<< " Este funcionário é classificado como gerente de projetos sênior";
+      cout<< " Este funcionário é classificado como gerente de projetos sênior.";
  }
     else if(salario > 15001 && salario <= 30000){
-      cout<< " Este funcionário é classificado como diretor de TI";
+      cout<< " Este funcionário é classificado como diretor de TI.";
  }
      else if(salario > 30001 && salario <= 50000){
-      cout<< " Este funcionário é classificado como diretor de TI sênor";
+      cout<< " Este funcionário é classificado como diretor de TI sênor.";
  }
- else {cout<< " Este é o Presidente da empresa";
+ else {cout<< " Este é o Presidente da empresa.";
      
  }
     return 0;
@@ -119,5 +124,3 @@ int main(){
     double desconto = 0;
     return cadastro (nome, cpf, nacionalidade, identidade, pais, estado, cidade, rua, numero, complemento, salario, proximo, desconto);
 }
-
-
